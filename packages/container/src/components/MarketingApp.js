3f1765a442bfec3eb2175395bdbb42aa1d90e4f9
Marcelo Alejandro <msv_326@outlook.com>
2022-04-15 00:34:14 -0400

    add container project

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,25 @@
+import { mount } from 'marketing/MarketingApp';
+import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+   const { onParentNavigate } = mount(ref.current, {
+     initialPath: history.location.pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        console.log(pathname, nextPathname);
+        if(pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      }
+    });
+
+    history.listen(onParentNavigate);
+  });
+
+  return <div ref={ref} />
+}
